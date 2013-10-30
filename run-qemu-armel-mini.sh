faeacf7b39226696b1bef18841871ec02610dbe5
Ralph.Chang <Ralph.Chang@Qisda.com>
2013-10-30 17:03:36 +0800

    Add ARM versatilepb support with kernel 2.6.x.

--- run-qemu-armel-mini.sh
+++ run-qemu-armel-mini.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# versatilepb booted with hda, kernel 2.6 with scsi disk support
+#qemu-system-arm -M versatilepb \
+    #-m 128M \
+    #-kernel zImage-2.6.25-versatile \
+    #-hda mini-rootfs-2.6.ext2 \
+    #-append "root=/dev/sda console=tty1 console=ttyAMA0" \
+    #-net nic -net user \
+    #-nographic
+
 # vexpress booted with sd, kernel 3.x
 # -redir tcp:5039::5039  --> TCP port 5039 on the host is redirected to the guest's port 5039
 qemu-system-arm -M vexpress-a9 \
